53c23c8e2772587f889db1b049fe8ef4bf3c0686
Ismael Martínez <77983091+SRC4R1M4NOL4@users.noreply.github.com>
2025-11-18 18:20:12 -0500

    commit para lab 18

---
+++ laboratorios/laboratorio18/laboratorio18/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace laboratorio181.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: AccessCobtroller
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Enter(String user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error :("+ ex.Message);
+            }
+        }
+    }
+}
